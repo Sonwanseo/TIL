3aaef03703717e20bd00a363feedbccd9a7d6f3f
Sonwanseo <sonwanseo@gmail.com>
2020-06-20 15:37:28 +0900

    #1.2 Actions/dispatch function gives reducer actions(Object)

--- React/VanillaRedux/src/index.js
+++ React/VanillaRedux/src/index.js
@@ -4,10 +4,22 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
+const countModifier = (count = 0, action) => {
+  if (action.type === "Add") {
+    return count + 1;
+  } else if (action.type === "Minus") {
+    return count - 1;
+  }
   return count;
 };
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "Add" });
+countStore.dispatch({ type: "Add" });
+countStore.dispatch({ type: "Add" });
+countStore.dispatch({ type: "Add" });
+countStore.dispatch({ type: "Add" });
+countStore.dispatch({ type: "Minus" });
 
 console.log(countStore.getState());
