0c04df874f0209018468a7b298d6169c4203905f
Sonwanseo <sonwanseo@gmail.com>
2021-01-26 15:27:10 +0900

    0.6

--- TypeScript/typechain/src/index.ts
+++ TypeScript/typechain/src/index.ts
@@ -1,7 +1,19 @@
-const sayHi = (name: string, age: number, gender: string): void => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}!`);
+interface Human {
+    name: string;
+    age: number;
+    gender: string;
+}
+
+const person = {
+    name: "Sonwanseo",
+    age: 19,
+    gender: "male",
+}
+
+const sayHi = (person: Human): string => {
+    return(`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`);
 };
 
-sayHi("Sonwanseo", 19, "male");
+console.log(sayHi(person));
 
 export {};
