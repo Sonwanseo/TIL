972a23739dc932deeafdca27db58bf2b930b9e09
Sonwanseo <sonwanseo@gmail.com>
2021-01-26 14:54:01 +0900

    0.4

--- TypeScript/typechain/index.ts
+++ TypeScript/typechain/index.ts
@@ -1,1 +1,9 @@
-console.log('hello')
+const name = "Sonwanseo", age = 19, gender = "male";
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}.`);
+};
+
+sayHi(name, age);
+
+export {};
