70252e2dc440ec61230764d73fc04de20ffe4e3d
Sonwanseo <sonwanseo@gmail.com>
2021-01-26 15:03:55 +0900

    0.5

--- TypeScript/typechain/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var name = "Sonwanseo", age = 19, gender = "male";
-var sayHi = function (name, age, gender) {
-    console.log("Hello " + name + ", you are " + age + ", you are a " + gender + ".");
-};
-sayHi(name, age);
--- TypeScript/typechain/index.ts
+++
@@ -1,9 +0,0 @@
-const name = "Sonwanseo", age = 19, gender = "male";
-
-const sayHi = (name, age, gender?) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}.`);
-};
-
-sayHi(name, age);
-
-export {};
